8593725f2b5b8a0a58312fdf53f7329336bea44b
Palina Chubina <palina1967038@gmail.com>
2023-06-27 13:54:48 +0300

    4_3 done 5_1 corrected data

--- 5_1.py
+++ 5_1.py
@@ -1,8 +1,8 @@
-number = 5
-multiplicity = 3
-start_number = 10
+number = int(input('Enter the number of output numbers: '))
+multiplicity = int(input('Enter multiplicity: '))
+start_number = int(input('Enter the starting number: '))
 counter = 0
-for i in range(5):
+for i in range(number):
     counter = start_number
     start_number += multiplicity
     print(counter, end='\t')
